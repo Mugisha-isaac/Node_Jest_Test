3fc65a8d2a58892fee8140ecf85b89fada0ab92d
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-01-22 09:26:18 +0200

    testing with not.toBe

--- functions.test.js
+++ functions.test.js
@@ -4,3 +4,7 @@ const functions = require('./functions');
 test('adding 2 to 2 to get 4',()=>{
     expect(functions.add(2,2)).toBe(4);
 })
+
+test('adding 2 to 2 to not  get 5',()=>{
+    expect(functions.add(2,2)).not.toBe(5);
+})
